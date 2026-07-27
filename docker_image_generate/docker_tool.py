0f991e6dec856b9b5f6540950bbb8ea33a14e11b
spider <149595555+SpiderSec101@users.noreply.github.com>
2026-07-28 01:17:07 +0530

    Update docker_tool.py

--- docker_image_generate/docker_tool.py
+++ docker_image_generate/docker_tool.py
@@ -516,25 +516,14 @@ def harvester():
     print("[+] Done theHarvester tool")
 
 
-
-def merge_files(file1, file2, output_file):
-    with open(output_file, "w", encoding="utf-8") as outfile:
-        for filename in (file1, file2):
-            with open(filename, "r", encoding="utf-8") as infile:
-                outfile.write(infile.read())
-
-
 def dnsgen():
     dnsgen_decision = input("[+] Do you want to run DNSGen? (y/n) => ")
     if dnsgen_decision == 'y':
         print("[+] Running Dnsgen, it will take some times, grab a coffee for yourself :-)")
         dnsgen_cmd = f"cat {base_dir}/{target_domain}/assets/Domains/live_subdomains.txt | dnsgen - | puredns resolve -r {working_dir}/resolvers.txt | sort -u > {base_dir}/{target_domain}/assets/Domains/dnsgen.txt"
         subprocess.run(dnsgen_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, text=True)
-        file_1 = f"{base_dir}/{target_domain}/assets/Domains/dnsgen.txt"
-        file_2 = f"{base_dir}/{target_domain}/assets/Domains/live_subdomains.txt"
-        main_file = f"{base_dir}/{target_domain}/assets/Domains/main.txt"
-        merge_files(file_1, file_2, main_file)
-
+        subprocess.run(f"cat {base_dir}/{target_domain}/assets/Domains/dnsgen.txt {base_dir}/{target_domain}/assets/Domains/live_subdomains.txt | cut -d '/' -f3 | sort -u > {base_dir}/{target_domain}/assets/Domains/main.txt", shell=True, text=True)
+        print("[+] DNSGen is done ;)")
     elif dnsgen_decision == 'n': print("[+] Skipped the tool Dnsgen")
     else: 
         print("[+] Not a valid option X")
